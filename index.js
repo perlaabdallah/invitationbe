8131ab31fd9f61d1ae1f16b4c334781a93650522
perlaabdallah <perlaabdallah@gmail.com>
2026-02-20 20:41:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 
   // Catch all handler for React Router
-  app.get("/*", (req, res) => {
+app.get("/:path*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build", "index.html"));
   });
 }
