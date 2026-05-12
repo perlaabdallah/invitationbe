f426e89fe357d188454664fac2b4ca50d3464d1a
perlaabdallah <perlaabdallah@gmail.com>
2026-05-12 14:50:46 +0300

    make attending required

--- model/familyModel.js
+++ model/familyModel.js
@@ -1,29 +1,32 @@
 import mongoose from "mongoose";
 
-const familySchema = new mongoose.Schema({
-  attendees: [
-    {
-      guestId: {
-        type: Number,
-        required: true,
+const familySchema = new mongoose.Schema(
+  {
+    attendees: [
+      {
+        guestId: {
+          type: Number,
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        attending: {
+          type: Boolean,
+          required: false,
+        },
       },
-      name: {
-        type: String,
-        required: true,
-      },
-      attending: {
-        type: Boolean,
-        required: true,
-      },
+    ],
+    giftRegistry: {
+      type: Boolean,
+      required: false,
+      default: true,
     },
-  ],
-  giftRegistry: {
-    type: Boolean,
-    required: false,
-    default: true,
   },
-}, {
-  timestamps: true, // Adds createdAt and updatedAt fields
-});
+  {
+    timestamps: true, // Adds createdAt and updatedAt fields
+  },
+);
 
 export default mongoose.model("Family", familySchema);
